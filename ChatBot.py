0818965016f5068f10783b9ec4252c9dddd468c6
XycnAzirul <124945374+XycnAzirul@users.noreply.github.com>
2023-02-10 10:26:09 +0700

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -17,10 +17,10 @@ def inline_menu():
     callback = types.InlineKeyboardButton(
         text="\U00002709 New chat", callback_data="NewChat"
     )
-    kenkan = types.InlineKeyboardButton(text="🔵 ᴏᴡɴᴇʀ", url=f"t.me/{OWNER}")
-    group = types.InlineKeyboardButton(text="👥 ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP}")
+    kenkan = types.InlineKeyboardButton(text="Owner", url=f"t.me/{OWNER}")
+    group = types.InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP}")
     channel = types.InlineKeyboardButton(
-        text="ᴄʜᴀɴɴᴇʟ 📣", url=f"https://t.me/{CHANNEL}"
+        text="ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{CHANNEL}"
     )
     menu = types.InlineKeyboardMarkup()
     menu.add(kenkan, channel, group, callback)
