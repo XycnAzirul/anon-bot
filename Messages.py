d8df182e7297399e80a7f5c29f6998fc0d44bfcb
XycnAzirul <124945374+XycnAzirul@users.noreply.github.com>
2023-02-10 11:39:10 +0700

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,34 +1,34 @@
-m_start = "Hai, selamat datang di Official Indonesia Chat\n\nSemoga Dapat teman atau jodoh\n\nkalau ada yang aneh-aneh lapor ke Azirul ya!\n\nTekan MULAI untuk memulai obrolannya:)\n\n-Bot oleh @azirul_fariq"
+m_start = "Hai, selamat datang di Official Indonesia Chat\n\nSemoga Dapat teman atau jodoh😊\n\nkalau ada yang aneh-aneh lapor ke Azirul ya!\n\nTekan MULAI untuk memulai obrolannya:)🏁\n\n-Bot oleh @azirul_fariq 👑"
 
 m_is_not_free_users = (
-    "Bersabar ya.. sedang mencari seseorang :)\n\nTekan /stop untuk menghentikan pencarian"
+    "Bersabar ya.. sedang mencari seseorang 😊\n\nTekan /stop untuk menghentikan pencarian"
 )
 
 m_is_connect = "✅ Kamu udah di obrolan, silahkan mengobrol!"
 
-m_play_again = "Apakah kamu mau mengobrol dengan orang lain?"
+m_play_again = "Apakah kamu mau mengobrol dengan orang lain?😊"
 
 m_is_not_user_name = "❌ Maaf, kamu harus mengisi Username profil kamu"
 
 m_good_bye = "❌ Kamu stop percakapannya"
 
-m_disconnect_user = "❌ Yah.. dia nge stop kamu :("
+m_disconnect_user = "😒 Yah.. dia nge stop kamu :("
 
 m_failed = "❌ Terjadi kesalahan!"
 
 m_like = "✅ Kamu mengirimkan username kepada lawan bicara!"
 
-m_dislike_user = "Obrolan Telah Berakhir"
+m_dislike_user = "❌ Obrolan Telah Berakhir"
 
 m_dislike_user_to = (
     "⛔ Lawan bicara kamu meninggalkan obrolan"
 )
 
-m_send_some_messages = "Gaboleh forward pesan dari bot lainnya!"
+m_send_some_messages = "❌ Gaboleh forward pesan dari bot lainnya!"
 
 m_has_not_dialog = "❌ Kamu lagi tidak ada di obrolan"
 
-dislike_str = "⛔ Keluar"    
+dislike_str = "⛔ Stop percakapan"    
 
 like_str = "📠 Kirim Kontak"
 
